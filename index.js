0a7187bd27e62c0ea268fa1b7300930d92d959b7
nickfrahm <nfrahm24@gmail.com>
2021-04-26 19:38:21 -0500

    trying to figure out how to start the JS portion

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-let input = "";
-let output = "0";
+const Calculator = {
+    input: "",
+    output: "",
+    decimal: false,
+    currentOperation: ""
+};
+
+let button = document.addEventListener("click", ()=> setInput());
 
 function add(a, b) {
     return a+b;
-}
+};
 
 function subtract(a,b) {
     return a-b;
-}
+};
 
 function multiply(a,b) {
     return a*b;
-}
+};
 
 function divide(a,b) {
     return a/b;
-}
+};
 
 function calculate(a,b) {
     
-}
+};
 
 function clearAll(a,b) {
     
-}
+};
 
 function deleteLastEntry(a,b) {
     
-}
+};
+
+function setInput() {
+
+};
