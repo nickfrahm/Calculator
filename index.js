fcda3b4c8a5cc0237f90aeea0bc259a297c6c7d9
nickfrahm <nfrahm24@gmail.com>
2021-04-26 20:47:58 -0500

    backspace, input, output, clear functionality

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const Calculator = {
     input: "",
-    output: "",
+    output: "0",
     decimal: false,
-    currentOperation: ""
+    operationValue: ""
 };
 
-let button = document.addEventListener("click", ()=> setInput());
+//allow input from clicking numbers
+const numbers = document.querySelectorAll("div.num");
+numbers.forEach((number) => number.addEventListener("click", () => setNumInput(number.innerHTML)));
 
-function add(a, b) {
-    return a+b;
+//allow for backspace of last input
+const backspace = document.getElementById("backspace");
+backspace.addEventListener("click", () => deleteLastEntry());
+
+//allow for clear of everything
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => clearAll());
+
+//giving operation buttons event handlers
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+
+add.addEventListener("click", () => add());
+subtract.addEventListener("click", () => subtract());
+multiply.addEventListener("click", () => multiply());
+divide.addEventListener("click", () => divide());
+
+//set the input as output
+function displayOutput() {
+    const outputText = document.querySelector("div.display-box");
+    Calculator.output = Calculator.input;
+    outputText.innerHTML = Calculator.output;
+}
+
+function add() {
+    Calculator.operationValue = 
+    Calculator.input = "0";
 };
 
 function subtract(a,b) {
@@ -27,14 +56,43 @@ function calculate(a,b) {
     
 };
 
-function clearAll(a,b) {
-    
+function clearAll() {
+    Calculator.input = "0";
+    Calculator.output = "0";
+    Calculator.decimal = false; 
+    Calculator.operationValue = "";
+    displayOutput();
 };
 
-function deleteLastEntry(a,b) {
-    
+function deleteLastEntry() {
+    let inputLength = Calculator.input.length;
+    let newInput = Calculator.input.slice(0, -1);
+    console.log(newInput);
+
+    if (inputLength > 1 && parseFloat(Calculator.input) > 0) {
+        console.log("positive backspace")
+        Calculator.input = newInput;
+        displayOutput();
+    } else if (inputLength > 2 && parseFloat(Calculator.input) < 0) {
+        console.log("negative backspace")
+        Calculator.input = newInput;
+        displayOutput();
+    } else {
+        console.log("only one digit")
+        Calculator.input = "0";
+        console.log(Calculator.input);
+        displayOutput();
+    }
 };
 
-function setInput() {
-
+function setNumInput(num) {
+    if (Calculator.input !== "0") {
+        Calculator.input += num;
+        console.log(Calculator.input);
+        displayOutput();
+    } else {
+        Calculator.input = num;
+        console.log(Calculator.input);
+        displayOutput();
+    }
 };
